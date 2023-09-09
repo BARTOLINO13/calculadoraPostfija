16a507825383916b92f4a952648aeda43fb8249b
Patricio <142331922+BARTOLINO13@users.noreply.github.com>
2023-09-08 19:35:38 -0600

    ALGORITMO CONEVRTIDOR COMPLETO

--- CALCULADORA_POSTFIJA/src/src/Algoritmo.java
+++ CALCULADORA_POSTFIJA/src/src/Algoritmo.java
@@ -16,34 +16,7 @@ public class Algoritmo {
 
     }
 
- 
-
-private int prioridades(Character op) {
-        int ans = -1;
-
-        switch (op) {
-            case '^':
-                ans = 3;
-                break;
-            case '*':
-            case '/':
-                ans = 2;
-                break;
-            case '+':
-            case '-':
-                ans = 1;
-                break;
-            case '(':
-                ans = 0;
-                break;
-        
-            default:
-                break;
-        }
-        return ans;
-    }
-
-    private boolean revisadorParentesis(String revisa) {
+    private static boolean revisadorParentesis(String revisa) {
         PilaA<Character> pila = new PilaA<>();
         boolean res = false;
         boolean ban = true;
@@ -68,86 +41,129 @@ private int prioridades(Character op) {
         }
         return res;
     }
-    
-    public ArrayList convertidor(String cad){
+
+    private static int prioridades(Character op) {
+        int ans = -1;
+
+        switch (op) {
+            case '^':
+                ans = 3;
+                break;
+            case '*':
+            case '/':
+                ans = 2;
+                break;
+            case '+':
+            case '-':
+                ans = 1;
+                break;
+            case '(':
+            case ')':
+                ans = 0;
+                break;
+
+            default:
+                break;
+        }
+        return ans;
+    }
+
+    public static ArrayList convertidor(String cad) {
         int i = 0;
-        int j = 0;
-        int r = 0;
+        double numero = 0;
         boolean band = true;
         ArrayList postfijo = new ArrayList();
-        Character ch,ch2,ch3;
+        Character ch;
         PilaA pila = new PilaA();
         StringBuilder sb = new StringBuilder();
-        
-        if(revisadorParentesis(cad))
-            while(i < cad.length()){
-                ch = cad.charAt(i);
-                
-                if(esOp(ch)== true)
-                    postfijo.add(ch);
-                else
-                    if(ch == '(')
+
+        while (i < cad.length()) {
+            ch = cad.charAt(i);
+
+            if (prioridades(ch) < 0) {
+                sb.append(ch);
+            } else {
+                switch (ch) {
+                    case '(':
                         pila.push(ch);
-                        else
-                    if(ch== ')'){
-                      while((Character) pila.peek()!= '('){
-                                  postfijo.add(pila.pop());
-                                  pila.push(ch);
-                                  pila.pop();
-                                  
+                        break;
+                    case ')':
+                        while ((Character) pila.peek() != '(') {
+                            numero = Double.parseDouble(sb.toString());
+                            sb.setLength(0);
+                            postfijo.add(numero);
+                            postfijo.add(pila.pop());
+                            pila.push(ch);
+                            pila.pop();
                         }
-                      pila.pop();
-                     
-                                  
-                    }
-                    
-                        else
-                             if(pila.isEmpty())
-                                pila.push(ch);
-                             else{
-                                 while(!pila.isEmpty() && prioridades((Character) pila.peek()) >= prioridades(ch))
-                                           postfijo.add(pila.pop());
-                
-                                           pila.push(ch);
-                             }
-                i++;
+                        pila.pop();
+                        break;
+                    default:
+                        if (sb.length() != 0) {
+                            numero = Double.parseDouble(sb.toString());
+                            sb.setLength(0);
+                            postfijo.add(numero);
+                        }
+                        if (pila.isEmpty()) {
+                            pila.push(ch);
+                        } else {
+                            while (!pila.isEmpty() && prioridades((Character) pila.peek()) >= prioridades(ch)) {
+                                postfijo.add(pila.pop());
+                            }
+                            pila.push(ch);
+                        }
+                        break;
+                }
             }
-        
-        while(!pila.isEmpty())
+            i++;
+
+        }
+        if (sb.length() != 0) {
+            numero = Double.parseDouble(sb.toString());
+            postfijo.add(numero);
+        }
+        while (!pila.isEmpty()) {
             postfijo.add(pila.pop());
-        
-        
-      
-        
-        
-     
-        
-        
+        }
         return postfijo;
     }
-    private boolean esOp(char c){
-        boolean resp = false;
-       switch(c){
-           case '0': 
-           case '1': 
-           case '2': 
-           case '3': 
-           case '4': 
-           case '5': 
-           case '6': 
-           case '7': 
-           case '8': 
-           case '9': 
-               resp = true;
-               break;
-               
-                 
-       }
-       return resp;
+
+    public static boolean checarSignos(String revisar) {
+        boolean correcto = true;
+        PilaA<Character> signos = new PilaA();
+        int tamanho, i;
+        Character signo;
+
+        tamanho = revisar.length();
+        i = 0;
+        while (i < tamanho && correcto) {
+            signo = revisar.charAt(i);
+            if (signo == '+' || signo == '*' || signo == '/' || signo == '^' || signo == '-') {
+                if (signos.isEmpty()) {
+                    signos.push(signo);
+                } else {
+                    correcto = false;
+                }
+            } else {
+                if (!signos.isEmpty()) {
+                    signos.pop();
+                }
+            }
+            i++;
+        }
+        if (!signos.isEmpty() && correcto) {
+            correcto = false;
+        }
+        return correcto;
     }
+    
     public static void main(String[] args) {
         Algoritmo algo = new Algoritmo();
-        
-        System.out.println(algo.convertidor("(2+2)*4"));
+
+        System.out.println(algo.convertidor("(1+2)*3"));
+        System.out.println(algo.convertidor("(1+2)/3"));
+        System.out.println(algo.convertidor("(1-2)*3"));
+        System.out.println(algo.convertidor("3*(1+2)"));
+        System.out.println(algo.convertidor("1+2*3"));
     }
 }
