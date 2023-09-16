61ead9df0b2a01a20775e1a2edbc63d80d9bb746
Patricio <142331922+BARTOLINO13@users.noreply.github.com>
2023-09-16 14:29:29 -0600

    Update Algoritmo.java
    CAMBIO COMPLETO PARA AJUSTARSE A VALORES NEGATIVOS,

--- CALCULADORA_POSTFIJA/src/src/Algoritmo.java
+++ CALCULADORA_POSTFIJA/src/src/Algoritmo.java
@@ -1,22 +1,53 @@
-/**
- *BARTOLINO
- *7 Sept 2023
- */
+
 package src;
 
-import static java.lang.Math.pow;
 import java.util.ArrayList;
 import pilas.PilaA;
+import pilas.PilaADT;
+import pilas.ExceptionColeccionVacia;
+
+/**
+ *
+ * @author maher
+ */
 
 public class Algoritmo {
-
-    ArrayList<Character> postfija;
-    PilaA<Character> pila;
-
-    public Algoritmo() {
-
+    private String operaciones;
+    
+    public Algoritmo(String operaciones){
+        this.operaciones=operaciones;
     }
 
+     public boolean revisadorPuntos(){//AFIRMA QUE LA SINTAXIS DE PUNTOS ESTÉ CORRECTA
+        boolean resp= true;
+        
+        boolean band= true;
+        boolean bandera= false;
+        int contPuntos=0;
+        int i=0;
+        
+        while(i< operaciones.length() && band){
+            if (operaciones.charAt(i) == '.'){
+                 contPuntos+=1;
+            }           
+            if(esOperador(operaciones.charAt(i)) || operaciones.charAt(i) == '!'){
+                bandera= true;
+            }           
+            if(contPuntos > 1 && !bandera){
+                band= false;
+            }
+            i++;
+        }                
+        if(band == true){
+            return  resp;
+        } else 
+            resp= false;
+                    return resp;
+        
+    }
+    
+    public boolean revisadorParentesis() {
+=======
     public static boolean revisadorPuntos(String revisa){
         boolean resp= true;
         
@@ -47,17 +78,18 @@ public class Algoritmo {
     }
     
     private static boolean revisadorParentesis(String revisa) {
+>>>>>>> origin/master
         PilaA<Character> pila = new PilaA<>();
         boolean res = false;
         boolean ban = true;
         int i = 0;
         Character ch;
 
-        while (i < revisa.length() && ban) {
-            if (revisa.charAt(i) == '(') {
-                ch = revisa.charAt(i);
+        while (i < operaciones.length() && ban) {
+            if (operaciones.charAt(i) == '(') {
+                ch = operaciones.charAt(i);
                 pila.push(ch);
-            } else if (revisa.charAt(i) == ')') {
+            } else if (operaciones.charAt(i) == ')') {
                 if (pila.isEmpty()) {
                     ban = false;
                 } else {
@@ -71,8 +103,56 @@ public class Algoritmo {
         }
         return res;
     }
+    
+    public boolean revisadorSignos() {
+        boolean correcto = true;
+        PilaA<Character> signos = new PilaA();
+        PilaA<Character> negativo = new PilaA();
+        int tamanho, i=0;
+        Character ch;
+        
+        tamanho=operaciones.length();
+        while(i<tamanho && correcto){
+            ch=operaciones.charAt(i);
+            if(esOperador(ch))
+                    if(!negativo.isEmpty() || !signos.isEmpty())
+                        correcto=false;
+                    else
+                        signos.push(ch);
+            else{
+                if(ch.equals('!')){
+                    if(!negativo.isEmpty())
+                        correcto=false;
+                    else
+                        negativo.push(ch);
+                }
+                else{
+                    if(ch != '('){
+                        if (!signos.isEmpty()) 
+                            signos.pop();
+                        if(!negativo.isEmpty())
+                            negativo.pop();
+                    }
+                }
+            }
+            i++;
+        }
+        if (!signos.isEmpty() && correcto) 
+            correcto = false;
+        if(!negativo.isEmpty() && correcto)
+            correcto =false;
+        return correcto;
+    }
+    
+    private boolean esOperador (Character ch){
+        boolean res=false;
+        
+        if(ch == '+' || ch == '*' || ch == '/' || ch == '^' || ch == '-')
+            res=true;
+        return res;
+    }
 
-    private static int prioridades(Character op) {
+    private int prioridades(Character op) {
         int ans = -1;
 
         switch (op) {
@@ -85,30 +165,30 @@ public class Algoritmo {
                 break;
             case '+':
             case '-':
+            case '!':
                 ans = 1;
                 break;
             case '(':
             case ')':
                 ans = 0;
                 break;
-
             default:
                 break;
         }
         return ans;
     }
 
-    public static ArrayList convertidor(String cad) {
-        int i = 0;
-        double numero = 0;
+    public ArrayList convertidor() {
+        int i=0;
+        double numero;
         ArrayList postfijo = new ArrayList();
         Character ch;
         PilaA pila = new PilaA();
         StringBuilder sb = new StringBuilder();
+        boolean bandera;
 
-        while (i < cad.length()) {
-            ch = cad.charAt(i);
-
+       while(i<operaciones.length()) {
+            ch = operaciones.charAt(i);
             if (prioridades(ch) < 0) {
                 sb.append(ch);
             } else {
@@ -117,15 +197,41 @@ public class Algoritmo {
                         pila.push(ch);
                         break;
                     case ')':
+                        System.out.println(sb.toString());
                         while ((Character) pila.peek() != '(') {
-                            numero = Double.parseDouble(sb.toString());
-                            sb.setLength(0);
-                            postfijo.add(numero);
+                            if(sb.length() != 0){
+                                numero = Double.parseDouble(sb.toString());
+                                sb.setLength(0);
+                                postfijo.add(numero);
+                            }
                             postfijo.add(pila.pop());
                             pila.push(ch);
                             pila.pop();
                         }
                         pila.pop();
+                        break;
+                    case '!':
+                        i++;
+                        ch = operaciones.charAt(i);
+                        if(operaciones.charAt(i)!= '('){
+                            bandera=true;
+                            while(prioridades(ch) < 0 && bandera){
+                                sb.append(ch);
+                                i++;
+                                if(i >= operaciones.length())
+                                    bandera=false;
+                                else
+                                    ch=operaciones.charAt(i);
+                            }   
+                            numero = Double.parseDouble(sb.toString());
+                            sb.setLength(0);
+                            numero = numero*-1;
+                            postfijo.add(numero);
+                        }
+                        else{
+                            pila.push('!');
+                        }
+                        i--;
                         break;
                     default:
                         if (sb.length() != 0) {
@@ -145,7 +251,6 @@ public class Algoritmo {
                 }
             }
             i++;
-
         }
         if (sb.length() != 0) {
             numero = Double.parseDouble(sb.toString());
@@ -156,47 +261,15 @@ public class Algoritmo {
         }
         return postfijo;
     }
-
-   public static boolean checarSignos(String revisar) {
-        boolean correcto = true;
-        PilaA<Character> signos = new PilaA();
-        int tamanho, i;
-        Character signo;
-
-        signo = revisar.charAt(0);
-        if(signo == '+' || signo == '*' || signo == '/' || signo == '^' || signo == '-')
-            correcto=false;
-        tamanho = revisar.length();
-        i = 0;
-        while (i < tamanho && correcto) {
-            signo = revisar.charAt(i);
-            if (signo == '+' || signo == '*' || signo == '/' || signo == '^' || signo == '-') {
-                if (signos.isEmpty()) {
-                    signos.push(signo);
-                } else {
-                    correcto = false;
-                }
-            } else {
-                if (!signos.isEmpty()) {
-                    signos.pop();
-                }
-            }
-            i++;
-        }
-        if (!signos.isEmpty() && correcto) {
-            correcto = false;
-        }
-        return correcto;
-    }
+    
     public double evalua(ArrayList postfijo) {
         double ans;
         double v;
         double x, y;
         PilaA<Double> pila = new PilaA();
         Character p;
-        for (int i = 0; i < postfijo.size(); i++) {
-            System.out.println(postfijo.get(i));
-
+        
+        for (int i = 0; i < postfijo.size(); i++){
             try {
                 pila.push((Double) postfijo.get(i));
             } catch (ClassCastException err) {
@@ -207,61 +280,60 @@ public class Algoritmo {
                     case '+':
                         x = (double) pila.pop();
                         y = (double) pila.pop();
-
                         v = x + y;
-
                         pila.push(v);
                         break;
                     case '-':
                         x = (double) pila.pop();
                         y = (double) pila.pop();
-
                         v = y - x;
-
                         pila.push(v);
                         break;
-
+                    case '!':
+                        x = (double) pila.pop();
+                        v = x*-1;
+                        pila.push(v);
+                        break;
                     case '*':
                         x = (double) pila.pop();
                         y = (double) pila.pop();
-
                         v = x * y;
-
                        pila.push(v);
                         break;
                     case '/':
                         x = (double) pila.pop();
                         y = (double) pila.pop();
-
                         v = y / x;
                         pila.push(v);
                         break;
-
                     case '^':
                         x = (double) pila.pop();
                         y = (double) pila.pop();
-                        v = pow(x, y);
+                        v = Math.pow(y, x);
                         pila.push(v);
                         break;
                 }
             }
 
         }
-
         ans = (double) pila.pop();
         return ans;
     }
 
     public static void main(String[] args) {
-        Algoritmo algo = new Algoritmo();
-
-        ArrayList lista = algo.convertidor("(1+2)*2");
-        System.out.println(algo.evalua(lista));
-//        System.out.println(algo.convertidor("(1+2)*3"));
-//        System.out.println(algo.convertidor("(1+2)/3"));
-//        System.out.println(algo.convertidor("(1-2)*3"));
-//        System.out.println(algo.convertidor("3*(1+2)"));
-//        System.out.println(algo.convertidor("1+2*3"));
-//        System.out.println(algo.checarSignos("1+*2"));
+        Algoritmo calc;
+        ArrayList convertidor;
+        
+        calc=new Algoritmo("!2^");
+        if(calc.revisadorParentesis() && calc.revisadorPuntos() && calc.revisadorSignos()){
+            convertidor=calc.convertidor();
+            System.out.println(convertidor);
+            System.out.println(calc.evalua(convertidor));
+        }
+        else
+            System.out.println("SYNTAX_ERROR");
     }
 }
+
+
+
